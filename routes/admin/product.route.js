9a58741ef73655a7a3189de5670ce78b07424ae4
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-20 22:51:09 +0700

    Update

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,17 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
 //const storageMulter = require("../../helpers/storageMulter.js")
 const multer  = require('multer')
 const upload = multer()
-
-cloudinary.config({ 
-    cloud_name: 'dpfnbbiq4', 
-    api_key: '821816283773499', 
-    api_secret: 'ibzKHBXf78NFOnYdOV1Q6325c-Q' // Click 'View API Keys' above to copy your API secret
-});
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware.js")
 
 const validate = require("../../validate/admin/product.validate.js")
 
@@ -30,35 +23,7 @@ router.get('/create', controller.create)
 
 router.post('/create', 
     upload.single('thumbnail'),
-    async function (req, res, next) {
-        if (req.file) {
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                      (error, result) => {
-                        if (result) {
-                          resolve(result);
-                        } else {
-                          reject(error);
-                        }
-                      }
-                    );
-                  streamifier.createReadStream(req.file.buffer).pipe(stream);
-                });
-            };
-
-            try {
-                let result = await streamUpload(req);
-                req.body[req.file.fieldname] = result.url;
-                next();  // Chuyển sang middleware tiếp theo
-            } catch (error) {
-                console.error('Lỗi upload:', error);
-                res.status(500).send({ error: 'Upload failed.' }); // Trả về lỗi nếu upload thất bại
-            }
-        } else {
-            next();  // Nếu không có file, chuyển sang middleware tiếp theo
-        }
-    }, 
+    uploadCloud.upload, 
     validate.createPost,
     controller.createPost
 );
