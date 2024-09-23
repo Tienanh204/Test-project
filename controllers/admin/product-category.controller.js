1818b7db18135ef3591deca855129b723e7560b1
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-23 20:35:36 +0700

    Update

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,7 @@
+// const Product = require("../../models/product.model.js")
+// const systemConfig = require("../../config/system.js")
+
+
+module.exports.index = async (req, res)=>{
+    res.send("oke")
+}
