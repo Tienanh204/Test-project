9057f26c883a932ed9fbc6a38820f564099f9f21
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-29 23:33:56 +0700

    Update

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,7 +1,76 @@
+const Roles = require("../../models/role.model")
 
 const systemConfig = require("../../config/system.js")
 const createTreeHelper = require("../../helpers/createTree.js")
+const Role = require("../../models/role.model")
 
-module.exports.index = async (req, res)=>{ 
-    res.send("oke")
+//1. [GET] admin/roles
+module.exports.index = async (req, res)=>{
+    const find = {
+        deleted: false
+    }
+    const records = await Roles.find(find)
+    res.render("admin/pages/roles/index.pug", {
+        pageTitle: "Nhóm quyền",
+        records: records
+    })
+}
+
+//2
+//[GET] admin/roles/create
+module.exports.create = async (req, res)=>{
+    const find = {
+        deleted: false
+    }
+    const records = await Roles.find(find)
+    res.render("admin/pages/roles/create.pug", {
+        pageTitle: "Tạo mhóm quyền",
+        records: records
+    })
+}
+
+//[POST] admin/roles/ceate
+module.exports.createPost = async (req, res)=>{
+    const role = new Role(req.body)
+    await role.save()
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
+}
+
+
+//3 Chi tiết nhóm quyền
+module.exports.detail = async (req, res)=>{
+    const find = {
+        deleted: false,
+        _id: req.params.id
+    }
+    const records = await Role.findOne(find)
+    res.render("admin/pages/roles/detail.pug", {
+        pageTitle: "Chi tiết mhóm quyền",
+        records: records
+    })
+}
+
+//4. Sửa nhóm quyền
+//[GET] admin/roles/edit
+module.exports.edit = async (req, res)=>{
+    const find = {
+        _id: req.params.id,
+        deleted: false
+    }
+    const records = await Roles.findOne(find)
+    res.render("admin/pages/roles/edit.pug", {
+        pageTitle: "Sửa nhóm quyền",
+        records: records
+    })
+}
+
+//[PATCH] admin/roles/editPatch
+module.exports.editPatch = async (req, res)=>{
+    try {
+        await Roles.updateOne({_id: req.params.id}, req.body);
+        req.flash('success', 'Cập nhập thành công');
+    } catch (error){
+        req.flash('error', 'Lỗi cập nhập!');
+    }
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -5,4 +5,15 @@ const controller = require("../../controllers/admin/role.controller")
 
 router.get('/', controller.index)
 
+//Tạo
+router.get('/create', controller.create)
+router.post('/create', controller.createPost)
+
+//Chi tiết
+router.get('/detail/:id', controller.detail)
+
+//Sửa
+router.get('/edit/:id', controller.edit)
+//router.patch('/edit/:id', controller.editPatch)
+
 module.exports = router
