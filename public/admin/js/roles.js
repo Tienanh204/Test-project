ac0d6cbe5557cc4839f0df910aa472036d50acba
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-10-06 22:15:48 +0700

    Update

--- public/admin/js/roles.js
+++ public/admin/js/roles.js
@@ -1,10 +1,42 @@
 
-const formChangePermission = document.querySelector("#form-change-permission")
-if(formChangePermission){
+const tablePermission = document.querySelector("[table-permission]")
+// console.log(tablePermission)
+if(tablePermission){
     const buttonSubmit = document.querySelector("[button-submit]") 
 
     buttonSubmit.addEventListener("click", ()=>{
         let permission = []
-        const rows = tablePermission.querySelectorAll("")
+        const rows = tablePermission.querySelectorAll("[data-name]")
+        
+        rows.forEach(row =>{
+            const name = row.getAttribute("data-name")
+            const inputs = row.querySelectorAll("input")
+            if(name == "id"){
+                inputs.forEach(input =>{
+                    const id = input.value;
+                    permission.push({
+                        id: id,
+                        permission: []
+                    })
+                })
+            }else{
+                inputs.forEach((input, index) =>{
+                    const checked = input.checked
+                    if(checked==true){
+                        permission[index].permission.push(name)
+                    }
+                })
+            }
+
+            if(permission.length > 0){
+                const formChangePermission = document.querySelector("#form-change-permission")
+                const inputPermission = formChangePermission.querySelector('input[name="permissions"]')
+                inputPermission.value = JSON.stringify(permission)
+
+                formChangePermission.submit()
+            }
+            // console.log(permission)
+        })
+
     })
 }
